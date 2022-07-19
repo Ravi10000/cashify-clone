53c28012b1711cd02f0abe1af7f780ab933b02b4
Ravi Sharma <ruravi80@gmail.com>
2022-07-19 13:55:30 +0530

    change app to start from homepage route to any route

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.use("/api/orders", orderRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
   });
 } else {
