4abc9a3e08b394e9ed3f40ed96874a4e5d195395
Ravi Sharma <ruravi80@gmail.com>
2023-01-04 21:45:53 +0530

    prepare to deploy

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 // import reportWebVitals from './reportWebVitals';
-import store from './redux/store';
-import { Provider } from 'react-redux'
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+if (process.env.NODE_ENV === "production") disableReactDevTools();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={store}>
-        <BrowserRouter>
-        <App />
-        </BrowserRouter>
-    </Provider>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
